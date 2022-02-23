b63d8a95ef1e591b39406a7cffe05289a23719bc
sarahcheng <iranalyzer01@mail.ksu.edu.tw>
2022-02-23 14:12:00 +0800

    [FIX]大部分金額對

--- 產學彙整系統應用_金額統計.sql
+++ 產學彙整系統應用_金額統計.sql
@@ -1,72 +1,129 @@
 SELECT 單位, ISNULL([2],0) AS '科技部計畫', ISNULL([3],0) AS '教育部計畫', ISNULL([4],0) AS '推廣教育', ISNULL([5],0) AS '技術移轉案', ISNULL([6],0) AS '其他計畫'  FROM
 (
-SELECT  [PPS_Research_TYPE_認列類別].[認列類別ID],  SUBSTRING(PPS_Research_TABLE_AchievementPeople.cHCode,13,2)  AS 二級單位 ,PPS_Research_TABLE_AchievementPeople.[cUnit], PPS_Research_TYPE_Department.cName AS 單位
-,SUM(PPS_Research_TABLE_AchievementPeople.iUnitPercentMoney) AS 績效分配金額
-  FROM [PPS_Research_TABLE_Main]
-  INNER JOIN
-	PPS_Research_TABLE_AchievementPeople ON 
-	PPS_Research_TABLE_AchievementPeople.iMap2Main =  PPS_Research_TABLE_Main.iAutoIndex
-INNER JOIN PPS_Research_TYPE_Department ON PPS_Research_TABLE_AchievementPeople.cUnit = PPS_Research_TYPE_Department.cIndex
-INNER JOIN [dbo].[PPS_Research_TYPE_認列項目] ON [認列Map2PlanTypeIndex]=[PPS_Research_TABLE_Main].[cMap2PlanType]
-INNER JOIN [dbo].[PPS_Research_TYPE_認列類別] ON [PPS_Research_TYPE_認列類別].[認列類別ID] = [dbo].[PPS_Research_TYPE_認列項目].[認列類別ID]
-
-WHERE YEAR([dPlanStartDate]) =2021 
-GROUP BY [PPS_Research_TYPE_認列類別].[認列類別ID], SUBSTRING(PPS_Research_TABLE_AchievementPeople.cHCode,13,2) ,PPS_Research_TABLE_AchievementPeople.[cUnit], PPS_Research_TYPE_Department.cName
-
+SELECT [PPS_Research_TYPE_認列項目].[認列類別ID],TD.cName AS 單位, 
+SUM(A.iUnitPercentMoney) AS 績效分配金額
+FROM dbo.PPS_Research_TABLE_Main AS M 
+INNER JOIN dbo.PPS_Research_TYPE_PlanType AS TP ON M.cMap2PlanType = TP.cIndex 
+INNER JOIN [dbo].[PPS_Research_TYPE_認列項目] ON [認列Map2PlanTypeIndex]=M.[cMap2PlanType] 
+AND [PPS_Research_TYPE_認列項目].[認列類別ID] = 2
+INNER JOIN
+(
+SELECT A.iMap2Main, A.cHCode, M.cPlanDepartmentMap2Department AS cPlanDepartmentIndex, a.bIsMaster, A.iUnitPercent, A.iUnitPercentMoney
+FROM dbo.PPS_Research_TABLE_AchievementPeople AS A INNER JOIN
+    dbo.PPS_Research_TABLE_Main AS M ON A.iMap2Main = M.iAutoIndex AND A.bIsMaster = 1
 UNION
-SELECT  [PPS_Research_TYPE_認列類別].[認列類別ID],SUBSTRING(PPS_Research_TABLE_AchievementPeople.cHCode,13,2)  AS 二級單位, PPS_Research_TABLE_AchievementPeople.[cUnit], PPS_Research_TYPE_Department.cName AS 單位
-,SUM(PPS_Research_TABLE_AchievementPeople.iUnitPercentMoney) AS 績效分配金額
-  FROM [PPS_Research_TABLE_Main]
-  INNER JOIN
-	PPS_Research_TABLE_AchievementPeople ON 
-	PPS_Research_TABLE_AchievementPeople.iMap2Main =  PPS_Research_TABLE_Main.iAutoIndex
-INNER JOIN PPS_Research_TYPE_Department ON PPS_Research_TABLE_AchievementPeople.cUnit = PPS_Research_TYPE_Department.cIndex
-INNER JOIN [dbo].[PPS_Research_TYPE_認列項目] ON [認列Map2PlanTypeIndex]=[PPS_Research_TABLE_Main].[cMap2PlanType]
-INNER JOIN [dbo].[PPS_Research_TYPE_認列類別] ON [PPS_Research_TYPE_認列類別].[認列類別ID] = [dbo].[PPS_Research_TYPE_認列項目].[認列類別ID]
+SELECT A.iMap2Main, A.cHCode, C.cUnitInside AS cPlanDepartmentIndex, a.bIsMaster, A.iUnitPercent, A.iUnitPercentMoney
+FROM dbo.PPS_Research_TABLE_AchievementPeople AS A INNER JOIN
+    dbo.PPS_Research_TABLE_CoMaster AS C ON A.iMap2Main = C.iMap2Main AND A.bIsMaster = 0 
+	AND A.cHCode = C.cHCodeInside AND A.cUnit = C.cUnitInside
 
-WHERE YEAR([dPlanStartDate]) =2021 AND [PPS_Research_TYPE_認列類別].[認列類別ID]=3
-GROUP BY [PPS_Research_TYPE_認列類別].[認列類別ID], SUBSTRING(PPS_Research_TABLE_AchievementPeople.cHCode,13,2),PPS_Research_TABLE_AchievementPeople.[cUnit], PPS_Research_TYPE_Department.cName
-
-UNION
-SELECT  [PPS_Research_TYPE_認列類別].[認列類別ID],SUBSTRING(PPS_Research_TABLE_AchievementPeople.cHCode,13,2)  AS 二級單位, PPS_Research_TABLE_AchievementPeople.[cUnit], PPS_Research_TYPE_Department.cName AS 單位
-,SUM(PPS_Research_TABLE_AchievementPeople.iUnitPercentMoney) AS 績效分配金額
- FROM [PPS_Research_TABLE_Main]
-  INNER JOIN
-	PPS_Research_TABLE_AchievementPeople ON 
-	PPS_Research_TABLE_AchievementPeople.iMap2Main =  PPS_Research_TABLE_Main.iAutoIndex
-INNER JOIN PPS_Research_TYPE_Department ON PPS_Research_TABLE_AchievementPeople.cUnit = PPS_Research_TYPE_Department.cIndex
-INNER JOIN [dbo].[PPS_Research_TYPE_認列項目] ON [認列Map2PlanTypeIndex]=[PPS_Research_TABLE_Main].[cMap2PlanType]
-INNER JOIN [dbo].[PPS_Research_TYPE_認列類別] ON [PPS_Research_TYPE_認列類別].[認列類別ID] = [dbo].[PPS_Research_TYPE_認列項目].[認列類別ID]
-WHERE YEAR([dPlanStartDate]) =2021 AND [PPS_Research_TYPE_認列類別].[認列類別ID]=4
-GROUP BY [PPS_Research_TYPE_認列類別].[認列類別ID], SUBSTRING(PPS_Research_TABLE_AchievementPeople.cHCode,13,2),PPS_Research_TABLE_AchievementPeople.[cUnit], PPS_Research_TYPE_Department.cName
+)AS A ON M.iAutoIndex = A.iMap2Main AND M.bPlanMoneyIntoSchool = 1 AND M.bPlanStateCheck = 1 
+INNER JOIN
+dbo.PPS_Research_TYPE_Department AS TD ON A.cPlanDepartmentIndex = TD.cIndex
+WHERE  YEAR(M.[dPlanStartDate]) =2021 
+GROUP BY [PPS_Research_TYPE_認列項目].[認列類別ID], TD.cName
 
 UNION
 
-SELECT  [PPS_Research_TYPE_認列類別].[認列類別ID], SUBSTRING(PPS_Research_TABLE_AchievementPeople.cHCode,13,2)  AS 二級單位,PPS_Research_TABLE_AchievementPeople.[cUnit], PPS_Research_TYPE_Department.cName AS 單位
-,SUM(PPS_Research_TABLE_AchievementPeople.iUnitPercentMoney) AS 績效分配金額
-  FROM [PPS_Research_TABLE_Main]
-  INNER JOIN
-	PPS_Research_TABLE_AchievementPeople ON 
-	PPS_Research_TABLE_AchievementPeople.iMap2Main =  PPS_Research_TABLE_Main.iAutoIndex
-INNER JOIN PPS_Research_TYPE_Department ON PPS_Research_TABLE_AchievementPeople.cUnit = PPS_Research_TYPE_Department.cIndex
-INNER JOIN [dbo].[PPS_Research_TYPE_認列項目] ON [認列Map2PlanTypeIndex]=[PPS_Research_TABLE_Main].[cMap2PlanType]
-INNER JOIN [dbo].[PPS_Research_TYPE_認列類別] ON [PPS_Research_TYPE_認列類別].[認列類別ID] = [dbo].[PPS_Research_TYPE_認列項目].[認列類別ID]
-WHERE YEAR([dPlanStartDate]) =2021 AND [PPS_Research_TYPE_認列類別].[認列類別ID]=5
-GROUP BY [PPS_Research_TYPE_認列類別].[認列類別ID], SUBSTRING(PPS_Research_TABLE_AchievementPeople.cHCode,13,2),PPS_Research_TABLE_AchievementPeople.[cUnit], PPS_Research_TYPE_Department.cName
+SELECT [PPS_Research_TYPE_認列項目].[認列類別ID],TD.cName AS 單位, 
+SUM(A.iUnitPercentMoney) AS 績效分配金額
+FROM dbo.PPS_Research_TABLE_Main AS M 
+INNER JOIN dbo.PPS_Research_TYPE_PlanType AS TP ON M.cMap2PlanType = TP.cIndex 
+INNER JOIN [dbo].[PPS_Research_TYPE_認列項目] ON [認列Map2PlanTypeIndex]=M.[cMap2PlanType] 
+AND [PPS_Research_TYPE_認列項目].[認列類別ID] = 3
+INNER JOIN
+(
+SELECT A.iMap2Main, A.cHCode, M.cPlanDepartmentMap2Department AS cPlanDepartmentIndex, a.bIsMaster, A.iUnitPercent, A.iUnitPercentMoney
+FROM dbo.PPS_Research_TABLE_AchievementPeople AS A INNER JOIN
+    dbo.PPS_Research_TABLE_Main AS M ON A.iMap2Main = M.iAutoIndex AND A.bIsMaster = 1
+UNION
+SELECT A.iMap2Main, A.cHCode, C.cUnitInside AS cPlanDepartmentIndex, a.bIsMaster, A.iUnitPercent, A.iUnitPercentMoney
+FROM dbo.PPS_Research_TABLE_AchievementPeople AS A INNER JOIN
+    dbo.PPS_Research_TABLE_CoMaster AS C ON A.iMap2Main = C.iMap2Main AND A.bIsMaster = 0 
+	AND A.cHCode = C.cHCodeInside AND A.cUnit = C.cUnitInside
+
+)AS A ON M.iAutoIndex = A.iMap2Main AND M.bPlanMoneyIntoSchool = 1 AND M.bPlanStateCheck = 1 
+INNER JOIN
+dbo.PPS_Research_TYPE_Department AS TD ON A.cPlanDepartmentIndex = TD.cIndex
+WHERE  YEAR(M.[dPlanStartDate]) =2021 
+GROUP BY [PPS_Research_TYPE_認列項目].[認列類別ID], TD.cName
+
 
 UNION
 
-SELECT  [PPS_Research_TYPE_認列類別].[認列類別ID], SUBSTRING(PPS_Research_TABLE_AchievementPeople.cHCode,13,2)  AS 二級單位,PPS_Research_TABLE_AchievementPeople.[cUnit], PPS_Research_TYPE_Department.cName AS 單位
-,SUM(PPS_Research_TABLE_AchievementPeople.iUnitPercentMoney) AS 績效分配金額
-  FROM [PPS_Research_TABLE_Main]
-  INNER JOIN
-	PPS_Research_TABLE_AchievementPeople ON 
-	PPS_Research_TABLE_AchievementPeople.iMap2Main =  PPS_Research_TABLE_Main.iAutoIndex
-INNER JOIN PPS_Research_TYPE_Department ON PPS_Research_TABLE_AchievementPeople.cUnit = PPS_Research_TYPE_Department.cIndex
-INNER JOIN [dbo].[PPS_Research_TYPE_認列項目] ON [認列Map2PlanTypeIndex]=[PPS_Research_TABLE_Main].[cMap2PlanType]
-INNER JOIN [dbo].[PPS_Research_TYPE_認列類別] ON [PPS_Research_TYPE_認列類別].[認列類別ID] = [dbo].[PPS_Research_TYPE_認列項目].[認列類別ID]
-WHERE YEAR([dPlanStartDate]) =2021 AND [PPS_Research_TYPE_認列類別].[認列類別ID]=6
-GROUP BY [PPS_Research_TYPE_認列類別].[認列類別ID], SUBSTRING(PPS_Research_TABLE_AchievementPeople.cHCode,13,2),PPS_Research_TABLE_AchievementPeople.[cUnit], PPS_Research_TYPE_Department.cName
+SELECT [PPS_Research_TYPE_認列項目].[認列類別ID],TD.cName AS 單位, 
+SUM(A.iUnitPercentMoney) AS 績效分配金額
+FROM dbo.PPS_Research_TABLE_Main AS M 
+INNER JOIN dbo.PPS_Research_TYPE_PlanType AS TP ON M.cMap2PlanType = TP.cIndex 
+INNER JOIN [dbo].[PPS_Research_TYPE_認列項目] ON [認列Map2PlanTypeIndex]=M.[cMap2PlanType] 
+AND [PPS_Research_TYPE_認列項目].[認列類別ID] = 4
+INNER JOIN
+(
+SELECT A.iMap2Main, A.cHCode, M.cPlanDepartmentMap2Department AS cPlanDepartmentIndex, a.bIsMaster, A.iUnitPercent, A.iUnitPercentMoney
+FROM dbo.PPS_Research_TABLE_AchievementPeople AS A INNER JOIN
+    dbo.PPS_Research_TABLE_Main AS M ON A.iMap2Main = M.iAutoIndex AND A.bIsMaster = 1
+UNION
+SELECT A.iMap2Main, A.cHCode, C.cUnitInside AS cPlanDepartmentIndex, a.bIsMaster, A.iUnitPercent, A.iUnitPercentMoney
+FROM dbo.PPS_Research_TABLE_AchievementPeople AS A INNER JOIN
+    dbo.PPS_Research_TABLE_CoMaster AS C ON A.iMap2Main = C.iMap2Main AND A.bIsMaster = 0 
+	AND A.cHCode = C.cHCodeInside AND A.cUnit = C.cUnitInside
+
+)AS A ON M.iAutoIndex = A.iMap2Main AND M.bPlanMoneyIntoSchool = 1 AND M.bPlanStateCheck = 1 
+INNER JOIN
+dbo.PPS_Research_TYPE_Department AS TD ON A.cPlanDepartmentIndex = TD.cIndex
+WHERE  YEAR(M.[dPlanStartDate]) =2021 
+GROUP BY [PPS_Research_TYPE_認列項目].[認列類別ID], TD.cName
+
+
+UNION
+
+SELECT [PPS_Research_TYPE_認列項目].[認列類別ID],TD.cName AS 單位, 
+SUM(A.iUnitPercentMoney) AS 績效分配金額
+FROM dbo.PPS_Research_TABLE_Main AS M 
+INNER JOIN dbo.PPS_Research_TYPE_PlanType AS TP ON M.cMap2PlanType = TP.cIndex 
+INNER JOIN [dbo].[PPS_Research_TYPE_認列項目] ON [認列Map2PlanTypeIndex]=M.[cMap2PlanType] 
+AND [PPS_Research_TYPE_認列項目].[認列類別ID] = 5
+INNER JOIN
+(
+SELECT A.iMap2Main, A.cHCode, M.cPlanDepartmentMap2Department AS cPlanDepartmentIndex, a.bIsMaster, A.iUnitPercent, A.iUnitPercentMoney
+FROM dbo.PPS_Research_TABLE_AchievementPeople AS A INNER JOIN
+    dbo.PPS_Research_TABLE_Main AS M ON A.iMap2Main = M.iAutoIndex AND A.bIsMaster = 1
+UNION
+SELECT A.iMap2Main, A.cHCode, C.cUnitInside AS cPlanDepartmentIndex, a.bIsMaster, A.iUnitPercent, A.iUnitPercentMoney
+FROM dbo.PPS_Research_TABLE_AchievementPeople AS A INNER JOIN
+    dbo.PPS_Research_TABLE_CoMaster AS C ON A.iMap2Main = C.iMap2Main AND A.bIsMaster = 0 
+	AND A.cHCode = C.cHCodeInside AND A.cUnit = C.cUnitInside
+
+)AS A ON M.iAutoIndex = A.iMap2Main AND M.bPlanMoneyIntoSchool = 1 AND M.bPlanStateCheck = 1 
+INNER JOIN
+dbo.PPS_Research_TYPE_Department AS TD ON A.cPlanDepartmentIndex = TD.cIndex
+WHERE  YEAR(M.[dPlanStartDate]) =2021 
+GROUP BY [PPS_Research_TYPE_認列項目].[認列類別ID], TD.cName
+
+UNION
+
+SELECT [PPS_Research_TYPE_認列項目].[認列類別ID],TD.cName AS 單位, 
+SUM(A.iUnitPercentMoney) AS 績效分配金額
+FROM dbo.PPS_Research_TABLE_Main AS M 
+INNER JOIN dbo.PPS_Research_TYPE_PlanType AS TP ON M.cMap2PlanType = TP.cIndex 
+INNER JOIN [dbo].[PPS_Research_TYPE_認列項目] ON [認列Map2PlanTypeIndex]=M.[cMap2PlanType] 
+AND [PPS_Research_TYPE_認列項目].[認列類別ID] = 6
+INNER JOIN
+(
+SELECT A.iMap2Main, A.cHCode, M.cPlanDepartmentMap2Department AS cPlanDepartmentIndex, a.bIsMaster, A.iUnitPercent, A.iUnitPercentMoney
+FROM dbo.PPS_Research_TABLE_AchievementPeople AS A INNER JOIN
+    dbo.PPS_Research_TABLE_Main AS M ON A.iMap2Main = M.iAutoIndex AND A.bIsMaster = 1
+UNION
+SELECT A.iMap2Main, A.cHCode, C.cUnitInside AS cPlanDepartmentIndex, a.bIsMaster, A.iUnitPercent, A.iUnitPercentMoney
+FROM dbo.PPS_Research_TABLE_AchievementPeople AS A INNER JOIN
+    dbo.PPS_Research_TABLE_CoMaster AS C ON A.iMap2Main = C.iMap2Main AND A.bIsMaster = 0 
+	AND A.cHCode = C.cHCodeInside AND A.cUnit = C.cUnitInside
+
+)AS A ON M.iAutoIndex = A.iMap2Main AND M.bPlanMoneyIntoSchool = 1 AND M.bPlanStateCheck = 1 
+INNER JOIN
+dbo.PPS_Research_TYPE_Department AS TD ON A.cPlanDepartmentIndex = TD.cIndex
+WHERE  YEAR(M.[dPlanStartDate]) =2021 
+GROUP BY [PPS_Research_TYPE_認列項目].[認列類別ID], TD.cName
 ) t
 PIVOT(
 SUM(績效分配金額)
